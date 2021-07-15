xakon/zoxide

Basic Makefile to control project

Makefile
@@ -1,0 +1,15 @@
+SHELL		:= /bin/sh
+
+PREFIX		?= /usr/local
+
+TARGET		:= zoxide
+
+
+install: release
+	cp target/release/${TARGET} ${DESTDIR}/${PREFIX}/bin/
+	strip ${DESTDIR}/${PREFIX}/bin/${TARGET}
+release:
+	cargo build --release
+
+
+.PHONY: release install

xakon/dog

Makefile
@@ -1,0 +1,15 @@
+SHELL		:= /bin/sh
+
+PREFIX		?= /usr/local
+
+TARGET		:= dog
+
+
+install: release
+	cp target/release/${TARGET} ${DESTDIR}/${PREFIX}/bin/
+	strip ${DESTDIR}/${PREFIX}/bin/${TARGET}
+release:
+	cargo build --release
+
+
+.PHONY: release install